c69b6e1a8cb9e3116a3d4cf3dbf0b52016268ce6
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-07-31 18:36:25 -0600

    finished any? enumerable exercises

--- exercises/any_pattern_test.rb
+++ exercises/any_pattern_test.rb
@@ -17,44 +17,43 @@ class AnyPatternTest < Minitest::Test
     numbers = [3, 1, 3, 2, 4, 9, 8]
     has_zero = false
     numbers.each do |number|
-      # Your code goes here
+      has_zero = true if number == 0
     end
     refute has_zero
   end
 
   def test_has_at_least_one_alice
-    skip
     names = ["Bill", "Bob", "Burton", "Alice", "Brandon"]
     has_alice = false
-    # Your code goes here
+    names.each {|name| has_alice = true if name == "Alice"}
     assert has_alice
   end
 
   def test_no_alices
-    skip
     names = ["Chuck", "Charlene", "Cory", "Chris", "Carl"]
-    # Your code goes here
+    has_alice = false
+    names.each {|name| has_alice = true if name == "Alice"}
     refute has_alice
   end
 
   def test_has_a_multi_word_phrase
-    skip
     phrases = ["Sure!", "OK.", "I have no idea.", "Really?Whatever."]
-    # Your code goes here
+    has_multi_word_phrase = false
+    phrases.each {|phrase| has_multi_word_phrase = true if phrase.include?(" ")}
     assert has_multi_word_phrase
   end
 
   def test_no_monkeys
-    skip
     animals = ["elephant", "hippo", "jaguar", "python"]
-    # Your code goes here
+    has_monkeys = true
+    animals.each {|animal| has_monkeys = false unless animal == "monkey" }
     refute has_monkeys
   end
 
   def test_no_multiples_of_five
-    skip
     numbers = [3, 1, 3, 2, 4, 9, 8]
-    # Your code goes here
+    multiples_of_5 = true
+    numbers.each {|number| multiples_of_5 = false unless number % 5 == 0 }
     refute multiples_of_5
   end
 
